de20412f9c6d2ce251b09af85034f4becfd544f7
Vortarin <mrpolovs@gmail.com>
2025-06-10 08:06:50 +0200

    Added some comments

--- lcd_printer.py
+++ lcd_printer.py
@@ -44,7 +44,17 @@ class LCDPrinter:
         self.print_title(st7789.WHITE)  # Print the title initially
         self.print_info("Waiting...", 10, self.FIRST_ROW_Y, st7789.WHITE, font=font_pretty)
 
+   
     def print_text(self, text, x, y, color=st7789.WHITE, font=font_schmol):
+        """
+        Print text on the TFT display at specified coordinates.
+        :param text: The text to print.
+        :param x: The x-coordinate for the text.
+        :param y: The y-coordinate for the text.
+        :param color: The color of the text (default is white).
+        :param font: The font to use for the text (default is font_schmol).
+        :return: None
+        """
         self.tft.text(font, text, x, y, color)
 
     def print_title(self, color=st7789.WHITE):
@@ -61,10 +71,18 @@ class LCDPrinter:
         self.print_text(text1, x1, 10, color, font=font_big)
         self.print_text(text2, x2, 10 + font_big.HEIGHT + 4, color, font=font_big)
 
+
     def print_info(self, text, x, y, color=st7789.WHITE, font=font_pretty):
         """
         Print information text on the TFT display, wrapping words to new lines if they would overflow.
+        :param text: The text to print.
+        :param x: The x-coordinate for the text.
+        :param y: The y-coordinate for the text.
+        :param color: The color of the text (default is white).
+        :param font: The font to use for the text (default is font_schmol).
+        :return: None
         """
+        
         screen_width = self.tft.physical_width
         words = text.split()
         lines = []
@@ -85,10 +103,16 @@ class LCDPrinter:
         for i, line in enumerate(lines):
             self.tft.text(font, line, x, y + i * (font.HEIGHT + 2), color)
 
+            
     def print_usage(self, cpu, ram, disk, msg=None):
         """
         Display CPU, RAM, and DISK usage on the TFT display.
+        :param cpu: cpu usage.
+        :param ram: ram usage.
+        :param disk: disk usage.
+        :param msg: additional message to print.
         """
+
         self.tft.fill(st7789.BLACK)
         # Center "Activity" and "Monitor" on separate lines
         self.print_title(st7789.WHITE)
@@ -96,7 +120,7 @@ class LCDPrinter:
         self.print_text(f" RAM: {ram}%", 10, self.FIRST_ROW_Y + font_schmol.HEIGHT + 4, st7789.GREEN, font=font_schmol)
         self.print_text(f"DISK: {disk}%", 10, self.FIRST_ROW_Y + 2*(font_schmol.HEIGHT + 4), st7789.BLUE, font=font_schmol)
         if msg:
-            self.print_text(msg, 10, self.FIRST_ROW_Y + 5*(font_schmol.HEIGHT + 4), st7789.WHITE, font=font_pretty)
+            self.print_info(msg, 10, self.FIRST_ROW_Y + 5*(font_schmol.HEIGHT + 4), st7789.WHITE, font=font_pretty)
 
 def main():
     printer = LCDPrinter()
